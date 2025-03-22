f4650abd8d1c38b7268cf326eb49896680dd2d84
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-03-22 11:02:27 +0300

    Fixed week01 visibility issue

---
+++ week01/Exercise5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayWelcome();
+        // Prompt the user for their name and favorite number
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        // Calculate and display the square of the user's favorite number
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function to display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function to get user's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function to get user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    //Function to square the number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    //Function to display the result
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+}
